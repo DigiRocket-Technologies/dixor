05a768552e04a70bf528674532b5457599f535c4
Nikhil Srivastava <nikhilsrv2017@gmail.com>
2025-04-09 13:08:52 +0530

    added social media marketing image and portfolio section

--- src/pages/innerPages/ProjectPage2.tsx
+++ src/pages/innerPages/ProjectPage2.tsx
@@ -2,22 +2,22 @@ import { Helmet } from "react-helmet-async";
 import Breadcrumb from "../../components/breadcrumb/Breadcrumb";
 import DarkClass from "../../components/classes/DarkClass";
 import LayoutV1 from "../../components/layouts/LayoutV1";
-import PortfolioV2 from "../../components/portfolio/PortfolioV2";
+import PortfolioV2New from "../../components/portfolio/PortfolioV2New";
 
-const ProjectPage = () => {
+const ProjectPage2 = () => {
     return (
         <>
             <Helmet>
-                <title>Dixor - Project</title>
+                <title>Portfolio | DigiRocket</title>
             </Helmet>
 
             <LayoutV1>
                 <Breadcrumb title='Portfolio' breadCrumb='Portfolio' />
-                <PortfolioV2 moreBtn={true} sectionClass='default-padding-bottom' />
+                <PortfolioV2New moreBtn={true} sectionClass='default-padding-bottom' />
                 <DarkClass />
             </LayoutV1>
         </>
     );
 };
 
-export default ProjectPage;
+export default ProjectPage2;
