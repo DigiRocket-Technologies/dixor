8d6643cde00bf3672da30cba43594dd64a0d55f2
Nikhil Srivastava <nikhilsrv2017@gmail.com>
2025-04-20 02:21:07 +0530

    wordpress pages

--- src/components/services/WordpressContent.tsx
+++ src/components/services/WordpressContent.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import PriceV2New from "../price/PriceV2New.tsx";
 import MostPopularServices from "./MostPopularServices.tsx";
 
+import {Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Keyboard, Navigation } from "swiper/modules";
+
 interface DataType {
   title?: string;
   bannerImg?: string;
@@ -63,36 +66,42 @@ const WordpressContent = ({
     {
       id: 1,
       title: "Open-Source Platform",
+      img:"/assets/img/services/wordpress/Open-Source Platform.png",
       description:
         "WordPress is free and fully customizable, with constant updates from a vibrant developer community.",
     },
     {
       id: 2,
       title: "User-Friendly Interface",
+      img:"/assets/img/services/wordpress/User-Friendly Interface.png",
       description:
         "WordPress is designed for ease of use, making it ideal for beginners. At the same time, it offers advanced features and flexibility for experienced developers.",
     },
     {
       id: 3,
       title: "SEO-Friendly",
+      img:"/assets/img/services/wordpress/SEO-Friendly.png",
       description:
         "With built-in SEO tools and plugins, WordPress helps you optimize your site for better visibility in search engines",
     },
     {
       id: 4,
       title: "Scalable and Flexible ",
+      img:"/assets/img/services/wordpress/Scalable and Flexible.png",
       description:
         "WordPress grows with your business, offering the flexibility to expand your site’s features as needed .",
     },
     {
       id: 5,
       title: "Wide Range of Themes",
+      img:"/assets/img/services/wordpress/Wide Range of Themes.png",
       description:
         "Choose from thousands of pre-designed themes to create a unique, professional look for your site .",
     },
     {
       id: 6,
       title: "Strong Community Support",
+      img:"/assets/img/services/wordpress/Strong Community Support.png",
       description:
         "Benefit from extensive resources, tutorials, and support from a large, active WordPress community.",
     },
@@ -131,81 +140,137 @@ const WordpressContent = ({
               <h1 style={{ marginBottom: "20px" }}>
                 Why WordPress is the Best Choice for Your Website?
               </h1>
-              <div className="row gx-1 mt-4 gy-4 mb-5">
-                {cards.map((card) => (
-                  <div key={card.id} className="col-12 mb-3 col-md-6 col-lg-4">
-                    <div className="card h-100 border shadow-sm">
-                      <div className="position-relative">
-                        <a href="/blog-single-with-sidebar/">
-                          <img
-                            src={`/assets/img/blog/4.jpg`}
-                            className="card-img-top"
-                            alt="Featured"
-                            style={{ height: "480px", objectFit: "cover" }}
-                          />
-                        </a>
-                        <div
-                          className="position-absolute bg-dark text-white rounded-circle d-flex align-items-center justify-content-center shadow"
-                          style={{
-                            width: "40px",
-                            height: "40px",
-                            top: "12px",
-                            left: "5px",
-                            fontWeight: "bold",
-                            border: "2px solid white",
-                          }}
-                        >
-                          {card.id}
+              <Swiper
+                loop={true}
+                centeredSlides={false}
+                autoplay={{
+                  delay: 5000,
+                  disableOnInteraction: false,
+                }}
+                speed={1000}
+                navigation={{
+                  nextEl: ".dropshippingservice-next",
+                  prevEl: ".dropshippingservice-prev",
+                }}
+                breakpoints={{
+                  0: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 30,
+                  },
+                  1000: {
+                    slidesPerView: 2.5,
+                    spaceBetween: 40,
+                  },
+                }}
+                modules={[Navigation, Keyboard, Autoplay]}
+              >
+                <div className="swiper-wrapper">
+                  {cards.map((card) => (
+                    <SwiperSlide key={card.id}>
+                      <div
+                        key={card.id}
+                        style={{ minHeight: "650px", padding: "10px" }}
+                      >
+                        <div className="card h-100 border">
+                          <div className="position-relative">
+                            <img
+                              src={
+                                card?.img ? card.img : `/assets/img/blog/4.jpg`
+                              }
+                              className="card-img-top"
+                              alt="Featured"
+                              style={{ height: "460px", width: "100%" }}
+                            />
+                            <div
+                              className="position-absolute bg-dark text-white rounded-circle d-flex align-items-center justify-content-center shadow"
+                              style={{
+                                width: "40px",
+                                height: "40px",
+                                top: "12px",
+                                left: "5px",
+                                fontWeight: "bold",
+                                border: "2px solid white",
+                              }}
+                            >
+                              {card.id}
+                            </div>
+                          </div>
+                          <div className="card-body">
+                            <h4
+                              style={{ color: "black" }}
+                              className="card-title"
+                            >
+                              {card.title}
+                            </h4>
+                            <p
+                              className="card-text text-muted"
+                              style={{
+                                overflow: "hidden",
+                                display: "-webkit-box",
+                                WebkitLineClamp: 3,
+                                WebkitBoxOrient: "vertical",
+                              }}
+                            >
+                              {card.description}
+                            </p>
+                          </div>
                         </div>
                       </div>
-                      <div className="card-body">
-                        <h4 style={{ color: "black" }} className="card-title">
-                          {card.title}
-                        </h4>
-                        <p
-                          className="card-text text-muted"
-                          style={{
-                            overflow: "hidden",
-                            display: "-webkit-box",
-                            WebkitLineClamp: 3,
-                            WebkitBoxOrient: "vertical",
-                          }}
-                        >
-                          {card.description}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+                    </SwiperSlide>
+                  ))}
+                </div>
+              </Swiper>
+              
+              <div
+                style={{
+                  display: "flex",
+                  marginTop: "30px",
+                  top: "110%",
+                  left: "50%",
+                  gap: "10px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <button className="dropshippingservice-prev left btn bg-white  border border-dark rounded-pill px-4 d-flex align-items-center">
+                  <i className="fas fa-chevron-left me-2 text-dark"></i>
+                </button>
+                <button className="dropshippingservice-next bg-white right btn  border border-dark rounded-pill px-4 d-flex align-items-center">
+                  <i className="fas fa-chevron-right ms-2 text-dark"></i>
+                </button>
               </div>
             </div>
             <div style={{ marginTop: "50px" }}>
               <h2 className="text-center">
                 Woocommerce integration for powerful ecommerce setup
               </h2>
-              <div className="mt-2">
-                <h2 className="mb-2">Seamless Integration with WordPress</h2>
+              <div className="mt-4">
+                <h3 className="mb-2">Seamless Integration with WordPress</h3>
                 <p>
                   WooCommerce integrates effortlessly with WordPress, providing
                   a strong platform for eCommerce.
                 </p>
               </div>
               <div className="mt-4">
-                <h2 className="mb-2">Customizable Product Options</h2>
+                <h3 className="mb-2">Customizable Product Options</h3>
                 <p>
                   WooCommerce allows you to create a variety of product types
                   and attributes, tailored to your business needs.
                 </p>
               </div>
               <div className="mt-4">
-                <h2 className="mb-2">Secure Payment Gateways</h2>
+                <h3 className="mb-2">Secure Payment Gateways</h3>
                 <p>
                   WooCommerce supports multiple secure payment gateways,
                   offering customers convenient and safe checkout options
                 </p>
               </div>
               <div className="mt-4">
-                <h2 className="mb-2">Scalable and Flexible</h2>
+                <h3 className="mb-2">Scalable and Flexible</h3>
                 <p>
                   WooCommerce grows with your business, offering features to
                   support everything from small shops to large enterprises.
