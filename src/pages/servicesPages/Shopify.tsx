3b1abdc1d8a41740e5986a612fee7f2a787f5e0b
Mahfooz Alam <98471514+Mahfoozalam1516@users.noreply.github.com>
2025-04-19 22:11:13 +0530

    Update Shopify.tsx

--- src/pages/servicesPages/Shopify.tsx
+++ src/pages/servicesPages/Shopify.tsx
@@ -1,41 +1,27 @@
 import Breadcrumb from "../../components/breadcrumb/Breadcrumb";
 import LayoutV1 from "../../components/layouts/LayoutV1";
 import ServicesV1Data from "../../assets/jsonData/services/ServiceV1New.json";
-import pricingInfo from "../../assets/jsonData/price/PriceV2New.json";
+import pricingInfo from "../../assets/jsonData/price/PriceV2New.json"
 import { Helmet } from "react-helmet-async";
 import DarkClass from "../../components/classes/DarkClass";
 import ShopifyContent from "../../components/services/ShopifyContent";
 
 const Shopify = () => {
-  const data = ServicesV1Data.find(
-    (service) => service.id === "shopify-development"
-  );
-  const pricing = pricingInfo.find(
-    (service) => service.serviceId === "shopify-development"
-  );
-  return (
-    <>
-      <Helmet>
-        <title>Shopify Development | DigiRocket Technologies</title>
-      </Helmet>
-
-      <LayoutV1>
-        <Breadcrumb
-          title="Shopify Development"
-          breadCrumb="Shopify Development"
-          LightMode={false}
-        />
-        {data && (
-          <ShopifyContent
-            serviceInfo={data}
-            pricing={pricing}
-            sectionClass="default-padding"
-          />
-        )}
-        <DarkClass />
-      </LayoutV1>
-    </>
-  );
+    const data = ServicesV1Data.find(service => service.id === "shopify-development")
+    const pricing=pricingInfo.find((service)=>service.serviceId==="shopify-development")
+    return (
+        <>
+            <Helmet>
+                <title>Shopify Development | DigiRocket Technologies</title>
+            </Helmet>
+            
+            <LayoutV1>
+                <Breadcrumb title='Shopify Development' breadCrumb='Shopify Development' LightMode={false} />
+                {data && <ShopifyContent serviceInfo={data} pricing={pricing} sectionClass='default-padding' />}
+                <DarkClass/>
+            </LayoutV1>
+        </>
+    );
 };
 
 export default Shopify;
