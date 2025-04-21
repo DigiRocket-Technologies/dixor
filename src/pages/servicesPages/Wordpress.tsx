994242d19454b6e1bc7461bf9ab08e0fda6aeee4
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2025-04-21 13:41:53 +0530

    wordpress spell fixed

--- src/pages/servicesPages/Wordpress.tsx
+++ src/pages/servicesPages/Wordpress.tsx
@@ -2,27 +2,41 @@ import Breadcrumb from "../../components/breadcrumb/Breadcrumb";
 import LayoutV1 from "../../components/layouts/LayoutV1";
 import ServicesV1Data from "../../assets/jsonData/services/ServiceV1New.json";
 import { Helmet } from "react-helmet-async";
-import pricingInfo from "../../assets/jsonData/price/PriceV2New.json"
+import pricingInfo from "../../assets/jsonData/price/PriceV2New.json";
 import WordpressContent from "../../components/services/WordpressContent";
 import DarkClass from "../../components/classes/DarkClass";
 
 const Wordpress = () => {
-    const data = ServicesV1Data.find(service => service.id === "wordpress-development");
-    const pricing=pricingInfo.find((service)=>service.serviceId==="wordpress-development")
+  const data = ServicesV1Data.find(
+    (service) => service.id === "wordpress-development"
+  );
+  const pricing = pricingInfo.find(
+    (service) => service.serviceId === "wordpress-development"
+  );
 
-    return (
-        <>
-            <Helmet>
-                <title>Wordpress Development | DigiRocket Technologies</title>
-            </Helmet>
-            
-            <LayoutV1>
-                <Breadcrumb title='Wordpress Development' breadCrumb='Wordpress Development' LightMode={false} />
-                {data && <WordpressContent serviceInfo={data} pricing={pricing} sectionClass='default-padding' />}
-                <DarkClass/>
-            </LayoutV1>
-        </>
-    );
+  return (
+    <>
+      <Helmet>
+        <title>WordPress Development | DigiRocket Technologies</title>
+      </Helmet>
+
+      <LayoutV1>
+        <Breadcrumb
+          title="WordPress Development"
+          breadCrumb="WordPress Development"
+          LightMode={false}
+        />
+        {data && (
+          <WordpressContent
+            serviceInfo={data}
+            pricing={pricing}
+            sectionClass="default-padding"
+          />
+        )}
+        <DarkClass />
+      </LayoutV1>
+    </>
+  );
 };
 
 export default Wordpress;
