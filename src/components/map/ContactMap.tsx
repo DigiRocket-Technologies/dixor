d42a331813bd099984742dc3577c364bcefcce88
dwivediapoorv <110631819+dwivediapoorv@users.noreply.github.com>
2025-04-21 23:32:13 +0530

    Update ContactMap.tsx

--- src/components/map/ContactMap.tsx
+++ src/components/map/ContactMap.tsx
@@ -5,6 +5,7 @@ const ContactMap = () => {
                 <div className="google-maps">
                     {/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48388.929990966964!2d-74.00332!3d40.711233!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1653598669477!5m2!1sen!2sus" /> */}
                     {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3093.836732527248!2d-75.52502155917644!3d39.155694944383605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c764aa6327e4c5%3A0x447e1926f48feac1!2s8%20The%20Green%2C%20Dover%2C%20DE%2019901%2C%20USA!5e0!3m2!1sen!2sin!4v1744219862823!5m2!1sen!2sin"></iframe> */}
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3800.2533461841053!2d77.53061576129417!3d29.968006282276363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sHotels!5e0!3m2!1sen!2sin!4v1745258366235!5m2!1sen!2sin"></iframe>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3509.1998489153125!2d77.04092437500033!3d28.4132259940067!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d193264f549fb%3A0xc393a21c6500fe64!2sDigirocket%20Technologies%20Pvt.%20Ltd.!5e0!3m2!1sen!2sin!4v1744220090260!5m2!1sen!2sin"></iframe>
                 </div>
             </div>
