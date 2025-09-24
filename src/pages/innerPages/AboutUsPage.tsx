541dfd7c7e6a3e36dbff5dbb53b05ef9d5a432af
DigiRocket-Technologies <digirockettechdepartment@gmail.com>
2025-09-24 15:36:14 +0000

    changes

--- src/pages/innerPages/AboutUsPage.tsx
+++ src/pages/innerPages/AboutUsPage.tsx
@@ -12,7 +12,7 @@ const AboutUsPage = () => {
     return (
         <>
             <Helmet>
-                <title>Dixor - About Uss</title>
+                <title>Dixor - About Us</title>
             </Helmet>
 
             <LayoutV1>
